540043fcbdff9b8e8198b4bb9953a534fca4a9ab
Захар Голуб <pahargolub2@gmail.com>
2025-06-16 21:43:49 +0300

    Кривое окно с уведомлением об ошибке

--- src/frontend/static/scripts/error_message.js
+++ src/frontend/static/scripts/error_message.js
@@ -1,5 +1,37 @@
-function createErrorNotification(){
-	const errorNotification = document.createElement('div');
-	errorNotification.setAttribute('visibility', 'collapse');
+function escapeHtml(unsafe){
+	return unsafe
+		.replace('&','&amp;')
+		.replace('<','&lt;')
+		.replace('>','&gt;')
+		.replace('\"','&quot;')
+		.replace('\'','&#39;')
+}
 
+var errorWindow;
+var errorWindowObject = {
+	htmlElement : errorWindow
+}
+
+function createErrorNotification(){
+	errorWindow = document.createElement('div');
+	errorWindow.style.visibility = 'collapse';
+	errorWindow.classList.add('error-window');
+	
+	errorWindow.innerHTML = `
+		<div class="error-window-element">
+			<h2 id="error-message"></h2>
+		</div>
+		<button class="error-window-element" onclick="hideError()" style = "">X</button>
+	`;
+
+	document.body.append(errorWindow);
+}
+
+
+function hideError(){
+	errorWindow.style.visibility='collapse';
+}
+function showError(error){
+	errorWindow.style.visibility='visible';
+	document.getElementById('error-message').textContent=escapeHtml(error);
 }
