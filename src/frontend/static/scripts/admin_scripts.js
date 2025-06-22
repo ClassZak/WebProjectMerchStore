6e86c8494b383e6586fa090f8fab1c8869c1745f
Захар Голуб <pahargolub2@gmail.com>
2025-06-22 18:43:30 +0300

    Описание прижимается к низу

--- src/frontend/static/scripts/admin_scripts.js
+++ src/frontend/static/scripts/admin_scripts.js
@@ -54,18 +54,20 @@ async function createGoodCard(element){
 
 	card.innerHTML = `
 		<div class="card-content">
-			<div class="one-photo">
+			<div class="one-image">
 				<img src="data:image/*;base64,${element.image.slice(2, -1)}" alt="${escapeHtml(element.name)}">
 			</div>
-			<h4 class="card-title">${escapeHtml(element.name)}</h4>
-			<h4 class="card-title">
-			${Intl.NumberFormat('ru-RU',{style: 'currency', currency: 'RUB'}).format(element.price)}
-			</h4>
-			<h5 class="card-title">${escapeHtml(element.description)}</h5>
-			<p>Производитель:</p>
-			<h4 class="card-title">${manufacturer && manufacturer.name ? manufacturer.name: 'Незвестен'}</h4>
-			<p>Дата появления в ассортименте:</p>
-			<h4 class="card-title">${element.appearance_date}</h4>
+			<div class="card-description">
+				<h4 class="card-title">${escapeHtml(element.name)}</h4>
+				<h4 class="card-title">
+				${Intl.NumberFormat('ru-RU',{style: 'currency', currency: 'RUB'}).format(element.price)}
+				</h4>
+				<h5 class="card-title">${escapeHtml(element.description)}</h5>
+				<p>Производитель:</p>
+				<h4 class="card-title">${manufacturer && manufacturer.name ? manufacturer.name: 'Незвестен'}</h4>
+				<p>Дата появления в ассортименте:</p>
+				<h4 class="card-title">${element.appearance_date}</h4>
+			</div>
 		</div>
 		<div class="card-button-div">
 			<button class="square-btn" onclick="updateGood(${element.id})"><strong>✎</strong></button>
@@ -91,7 +93,7 @@ async function loadManufacturersToArray(){
 	try{
 		if(!response.ok) {
 			message = (await response.json()).error
-			throw new Error(`Response status: ${response.status}\nError Message: ${message}`);
+			throw new Error(`Статус запроса: ${response.status}\nОшибка: ${message}`);
 		}
 
 		let elements = await response.json();
@@ -221,7 +223,7 @@ async function loadGoods() {
 		if(!response.ok)
 		{
 			message = (await response.json()).error;
-			throw new  Error(`Response status: ${response.status}\nError Message: ${message}`);
+			throw new  Error(`Статус запроса: ${response.status}\nОшибка: ${message}`);
 		}
 		
 		let elements = await response.json();
@@ -564,7 +566,7 @@ document.addEventListener('DOMContentLoaded', function() {
 				if (data.error) {
 					alert(data.error);
 				} else {
-					alert(`Успешно изменены данные товара \"${formDataObject.name})}\"`);
+					alert(`Успешно изменены данные товара \"${formDataObject.name}\"`);
 					closeModal('edit_goods_form_overlay');
 					e.target.reset();
 					loadGoods(); // Обновляем список товаров
--- src/frontend/static/scripts/goods_scripts.js
+++ src/frontend/static/scripts/goods_scripts.js
@@ -10,11 +10,29 @@ async function loadManufacturersToArray(){
 	try{
 		if(!response.ok) {
 			message = (await response.json()).error
-			throw new Error(`Response status: ${response.status}\nError Message: ${message}`);
+			throw new Error(`Статус запроса: ${response.status}\nОшибка: ${message}`);
 		}
 
 		let elements = await response.json();
 		manufacturers=elements.manufacturers;
+	}
+	catch(error){
+		console.log(error);
+		showError(message);
+	}
+}
+async function loadGoodsToArray(apiRoute='') {
+	let response = await fetch('/api/goods/'+apiRoute);
+
+	let message = '';
+	try{
+		if(!response.ok){
+			message = (await response.json()).error;
+			throw new Error(`Статус запроса: ${response.status}\nОшибка: ${message}`);
+		}
+
+		let elements = await response.json();
+		goods = elements.goods;
 	}
 	catch(error){
 		console.log(error);
@@ -36,72 +54,48 @@ async function createGoodCard(element){
 
 	card.innerHTML = `
 		<div class="card-content">
-			<div class="one-photo">
+			<div class="one-image">
 				<img src="data:image/*;base64,${element.image.slice(2, -1)}" alt="${escapeHtml(element.name)}">
 			</div>
-			<h4 class="card-title">${escapeHtml(element.name)}</h4>
-			<h4 class="card-title">
-			${Intl.NumberFormat('ru-RU',{style: 'currency', currency: 'RUB'}).format(element.price)}
-			</h4>
-			<h5 class="card-title">${escapeHtml(element.description)}</h5>
-			<p>Производитель:</p>
-			<h4 class="card-title">${manufacturer && manufacturer.name ? manufacturer.name: 'Незвестен'}</h4>
-			<p>Дата появления в ассортименте:</p>
-			<h4 class="card-title">${element.appearance_date}</h4>
+			<div class="card-description">
+				<h4 class="card-title">${escapeHtml(element.name)}</h4>
+				<h4 class="card-title">
+				${Intl.NumberFormat('ru-RU',{style: 'currency', currency: 'RUB'}).format(element.price)}
+				</h4>
+				<h5 class="card-title">${escapeHtml(element.description)}</h5>
+				<p>Производитель:</p>
+				<h4 class="card-title">${manufacturer && manufacturer.name ? manufacturer.name: 'Незвестен'}</h4>
+				<p>Дата появления в ассортименте:</p>
+				<h4 class="card-title">${element.appearance_date}</h4>
+			</div>
 		</div>
 	`
 
 	return card;
 }
 
-async function loadGoods(elementId) {
-	let response = await fetch('/api/goods/', {method: 'get'});
-
-	const container = document.getElementById(elementId);
-	container.innerHTML = '';
-
-	let message = '';
+async function loadGoods(elementId,apiRoute='') {
 	try {
-		if(!response.ok) {
-			message = (await response.json()).error;
-			throw new Error(`Response status: ${response.status}\nError Message: ${message}`);
-		}
+		const container = document.getElementById(elementId);
+		container.innerHTML = '';
+
+		await loadGoodsToArray(apiRoute);
 		
-		let elements = await response.json();
-		goods = elements.goods;
 		goods.forEach(async element => {
 			container.appendChild(await createGoodCard(element));
 		});
 	} catch (error) {
 		console.log(error);
-		showError(message);
 	}
 }
 async function loadNewGoods(elementId) {
-	try {
-		let response = await fetch('/api/goods/new/', {method: 'get'});
-
-		const container = document.getElementById(elementId);
-		if(!container)
-			throw new Error('Ошибка поиска элемента для загрузки элементов');
-		container.innerHTML = '';
-
-		let message = '';
-	
-		if(!response.ok) {
-			message = (await response.json()).error;
-			throw new Error(`Response status: ${response.status}\nError Message: ${message}`);
-		}
-		
-		let elements = await response.json();
-		goods = elements.goods;
-		goods.forEach(async element => {
-			container.appendChild(await createGoodCard(element));
-		});
-	} catch (error) {
-		console.log(error);
-		showError(message);
-	}
+	await loadGoods(elementId,'new/')
 }
 
 
+document.addEventListener('DOMContentLoaded',async function(params){
+	setTimeout(async function(){
+		await loadManufacturersToArray();
+		loadNewGoods('goods_grid');
+	}, 500);
+});
