5978e653f3edf6578efe17675fc9917616d94b9c
Захар Голуб <pahargolub2@gmail.com>
2025-06-22 23:54:45 +0300

    Обновление скрипта

--- setup_environment.sh
+++ setup_environment.sh
@@ -2,9 +2,10 @@
 
 sudo apt-get install mysql-server
 
-
+sudo apt install python3.12-venv
 python3 -m venv .venv
 . .venv/bin/activate
 
 
 pip install Flask mysql-connector-python
+pip install flask_wtf
